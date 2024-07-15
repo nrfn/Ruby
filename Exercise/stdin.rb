ee5d032a152ddaa2d0ba1ec6ba39e31bf03eb868
nrfn <59220732+nrfn@users.noreply.github.com>
2024-07-16 00:09:24 +0900

    splitについて学習。

--- Exercise/stdin.rb
+++ Exercise/stdin.rb
@@ -24,3 +24,11 @@ array = input_line.split(" ")
 array.each do |input|
   puts input
 end
+
+# N個のデータの入力
+input_line = gets
+input_words = gets
+array = input_words.split(" ")
+array.each do |input|
+  puts input
+end
